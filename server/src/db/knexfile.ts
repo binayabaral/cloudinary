c9a50657891ee1ee09830c19e6370b99d71a90d9
Binaya Baral <binaya.baral5@gmail.com>
2023-08-25 23:30:27 +0545

    fix: support unauthorized ssl certificate in dev

--- server/src/db/knexfile.ts
+++ server/src/db/knexfile.ts
@@ -13,6 +13,7 @@ const config: { [key: string]: Knex.Config } = {
       user: process.env.DB_USER,
       host: process.env.DB_HOST,
       password: process.env.DB_PASSWORD,
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
